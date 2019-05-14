16296c28dfb70e9a2c7551c70f253c37478ac298
Eduardo <efelipe10@hotmail.com>
2019-05-14 14:51:46 -0300

    - Editando

--- src/main/java/br/senac/tads/dsw/tarefas/controller/TarefaController.java
+++ src/main/java/br/senac/tads/dsw/tarefas/controller/TarefaController.java
@@ -7,6 +7,7 @@ import br.senac.tads.dsw.tarefas.repository.TarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -48,6 +49,37 @@ public class TarefaController {
         List<Tarefa> tarefas = tarefaRepository.findAll();
 
         mv.addObject("tarefas", tarefas);
+
+        return mv;
+    }
+
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable int id){
+        ModelAndView mv = new ModelAndView("Tela2A");
+
+        List<Pessoa> pessoas = pessoaRepository.findAll();
+        Tarefa t = tarefaRepository.getOne(id);
+
+        mv.addObject("tarefa", t);
+        mv.addObject("pessoas", pessoas);
+
+        return mv;
+    }
+
+    @PostMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable int id, Tarefa t){
+        ModelAndView mv = new ModelAndView("redirect:/index");
+
+        Tarefa tare = tarefaRepository.getOne(id);
+
+        tare.setDataUltimaAlteracao(LocalDateTime.now());
+        tare.setDescricao(t.getDescricao());
+        tare.setDataInicioPrevisto(t.getDataInicioPrevisto());
+        tare.setDataTerminoPrevisto(t.getDataTerminoPrevisto());
+        tare.setNome(t.getNome());
+        tare.setResponsavel(t.getResponsavel());
+
+        tarefaRepository.save(tare);
 
         return mv;
     }
